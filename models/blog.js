fcd5d30f47af9d8eb3074620558a86755e2c0c1e
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2023-11-17 18:38:29 +0530

    Update blog.js

--- models/blog.js
+++ models/blog.js
@@ -1,22 +1,20 @@
 import mongoose, { Schema } from "mongoose";
 
-const blogSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-    },
-    content: {
-      type: String,
-    },
-    coverImage: {
-      type: String,
-    },
-    createdBy: {
-      type: Schema.Types.ObjectID,
-      ref: "user",
-    },
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
   },
-);
+  content: {
+    type: String,
+  },
+  coverImage: {
+    type: String,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectID,
+    ref: "user",
+  },
+});
 
 const blog = new mongoose.model("blog", blogSchema);
 
